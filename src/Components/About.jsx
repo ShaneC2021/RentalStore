89f304e6c9d0f08b3338575bf74bfed8e2d13057
ShaneC2021 <84458099+ShaneC2021@users.noreply.github.com>
2022-02-22 14:20:08 -0330

    contact page updated to handle state

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,18 +4,20 @@ import Home from "./Home";
 
 function About() {
   return (
-    <div className="main-body d-flex  flex-column text-center ">
+    <div className="main-body d-flex flex-column align-items-center justify-content-center text-center p-2 ">
+      <div className="contact-details about">
       <div><h2>About Us</h2></div>
-      <div>
+      <div className="p-1">
       <p>
         We are a small unique family owned business dedicated to providing a
-        quality easy to use rental service to travellers with a sense of
+        quality easy to use rental service to travellers in the Arctic region with a sense of
         adventure. We specialize in Campervans both modern and vintage. Whether
         you are seeking a weekend get away or a 3 month expedition into the
         wild, we have a Camper for you. We look forward to assisting you in
         making that adventure of a lifetime become reality.
       </p>
       </div>
+    </div>
     </div>
   );
 }
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,30 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 function Contact() {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+  const [values, setValues] = useState(initialValues);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="main-body">
-      <div className="contact-details p-2 main-body">
-        <Form>
+    <div className=" main-body p-1 d-flex flex-column align-items-center justify-content-center">
+      <div className="contact-details p-2">
+        
+        <Form onSubmit={handleSubmit}>
           <Row className="mb-0">
             <Form.Group as={Col} controlId="firstName">
               <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" placeholder="First Name" required />
+              <Form.Control
+                type="text"
+                placeholder="First Name"
+                value={values.firstName}
+                onChange={handleChange}
+                name="firstName"
+                required
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId="lastName">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" placeholder="Last Name" required />
+              <Form.Control
+                type="text"
+                placeholder="Last Name"
+                value={values.lastName}
+                onChange={handleChange}
+                name="lastName"
+                required
+              />
             </Form.Group>
           </Row>
 
           <Row>
             <Form.Group className="mb-3" controlId="email">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" required />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={values.email}
+                onChange={handleChange}
+                name="email"
+                required
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -38,6 +78,9 @@ function Contact() {
                 type="tel"
                 placeholder="Phone Number"
                 pattern="[0-9]{1}-[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                value={values.phone}
+                onChange={handleChange}
+                name="phone"
                 required
               />
             </Form.Group>
@@ -46,7 +89,14 @@ function Contact() {
           <Row>
             <Form.Group className="mb-3" controlId="message">
               <Form.Label>Message</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control
+                as="textarea"
+                rows={3}
+                value={values.message}
+                onChange={handleChange}
+                name="message"
+                required
+              />
             </Form.Group>
           </Row>
           <div className="d-grid">
@@ -61,4 +111,3 @@ function Contact() {
 }
 
 export default Contact;
-
