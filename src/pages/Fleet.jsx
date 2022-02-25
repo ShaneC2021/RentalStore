9d2155c8ffd5a337026d0ca289e10a8e7ef4137a
ShaneC2021 <84458099+ShaneC2021@users.noreply.github.com>
2022-02-25 12:56:12 -0330

    mapped images from fake data into fleet component woohoo

--- src/pages/Fleet.jsx
+++ src/pages/Fleet.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import arrayOfVans from "../scripts/data";
+import {useState} from 'react';
+
+
+
 
 function Fleet() {
+  console.log(arrayOfVans);
   return (
     <div className="main-body-fleet">
       <Row xs={1} md={2} lg={4} className="g-3">
-        {Array.from({ length: 4}).map((_, idx) => (
+        {arrayOfVans.map((item, idx) => (
         
           <Col key={idx}>
             <Card>
-              <Card.Img variant="top" width="243" height="160"src={require(`../images/fleet${idx}.jpg`)} />
+              <Card.Img variant="top" width="243" height="160"src={require(`../images/${item.image}`)}  alt="pic of van" />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title>{item.name}</Card.Title>
                 <Card.Text>
-                  lknlfnv
+                 {item.description}
+                 <br />
+                 {item.price}
                 </Card.Text>
                 <Button variant="primary">Rent Me</Button>
               </Card.Body>
